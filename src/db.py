c325ae41eea28647879ce12121ffa49a4bd1e69c
FabGPT <fabgpt.inbox@gmail.com>
2026-04-23 12:10:23 +0200

    docs(src): add docstrings for public symbols in db.py [gitoma]

--- src/db.py
+++ src/db.py
@@ -1,8 +1,22 @@
 import psycopg2
 from psycopg2 import sql
 
+
 def find_user_by_name(conn, name):
-    query = sql.SQL("SELECT * FROM users WHERE name = {};").format(
+    """
+    Find a user by their name in the database.
+
+    Args:
+        conn: A psycopg2 connection object to the database.
+        name (str): The name of the user to find.
+
+    Returns:
+        list: A list of database rows representing users with the given name.
+
+    Raises:
+        psycopg2.Error: If a database error occurs during query execution.
+    """
+    query = sql.SQL("SELECT * FROM users WHERE name = {}; ").format(
         sql.Literal(name)
     )
     with conn.cursor() as cursor:
